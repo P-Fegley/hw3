d13bc8722194c7905203a854f3c532179fe964de
Preston Fegley <fegley.preston@gmail.com>
2025-02-26 23:51:01 +0000

    small style/comment edits

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
 
   resources "entries"
 
+  # Quit server and run "rails generate controller [name]" for each resource
+  # Then code controllers and add index/show/new/etc files under 'views'
+
 end
