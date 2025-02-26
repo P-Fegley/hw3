bb5ff75176d4239df898ed3fb292c2b6fc105610
Preston Fegley <fegley.preston@gmail.com>
2025-02-26 23:14:54 +0000

    deleted commented-out code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,5 @@
 class EntriesController < ApplicationController
 
-  # def show
-  #   @entry = Entry.find_by({"id" => params["id"]})
-  #   @place = Place.find_by({"id" => @entry["place_id"]})
-  # end
-
   def new
   end
 
